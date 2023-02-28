abc710b814d12d4ca76c0a06b4955210e29dd642
Mikko Gynther <mgynthercode@gmail.com>
2023-02-28 20:43:34 +0200

    Remove backend-express
    Clean up buggy and hard to maintain express backend.

--- backend-express/src/server.ts
+++
@@ -1,9 +0,0 @@
-import App from '@/app';
-import AuthRoute from '@routes/auth.route';
-import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
-import validateEnv from '@utils/validateEnv';
-
-validateEnv();
-
-new App([new IndexRoute(), new UsersRoute(), new AuthRoute()], undefined);
