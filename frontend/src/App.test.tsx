dd388ba269885dae5f44f521b52f59f8df30c0e7
Mikko Gynther <mgynthercode@gmail.com>
2023-03-16 21:24:46 +0200

    Fix frontend example test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { store } from './store/store'
 
 test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+  const linkElement = screen.getByText(/Username/i)
   expect(linkElement).toBeInTheDocument()
 })
