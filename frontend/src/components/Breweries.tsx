4958cfe136fb5e130f21b1b0426befb26fc77840
Mikko Gynther <mgynthercode@gmail.com>
2023-04-30 16:19:09 +0300

    Add search brewery to breweries
    Search improves UX a lot. Later a proper server-side search
    and not loading full list will also improve performance.

--- frontend/src/components/Breweries.tsx
+++ frontend/src/components/Breweries.tsx
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { useListBreweriesQuery } from '../store/brewery/api'
 import { type Brewery } from '../store/brewery/types'
 
 import LoadingIndicator from './LoadingIndicator'
+import SearchBrewery from './SearchBrewery'
 
 function Breweries (): JSX.Element {
+  const navigate = useNavigate()
   const { data: breweryData, isLoading } = useListBreweriesQuery()
 
   const breweryArray = breweryData?.breweries === undefined
@@ -14,14 +16,21 @@ function Breweries (): JSX.Element {
   const breweries = breweryArray
     .sort((a, b) => a.name.localeCompare(b.name))
 
+  function toRoute (brewery: Brewery): string {
+    return `/breweries/${brewery.id}`
+  }
+
   return (
     <div>
       <h3>Breweries</h3>
       <LoadingIndicator isLoading={isLoading} />
+      <SearchBrewery select={(brewery) => {
+        navigate(toRoute(brewery))
+      }} />
       <ul>
         {breweries?.map((brewery: Brewery) => (
           <li key={brewery.id}>
-            <Link to={`/breweries/${brewery.id}`}>
+            <Link to={toRoute(brewery)}>
               {brewery.name}
             </Link>
           </li>
