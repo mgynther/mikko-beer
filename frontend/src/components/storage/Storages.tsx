36444f2fe80f3f2955a2204332083a2e97c4f274
Mikko Gynther <mgynthercode@gmail.com>
2023-09-23 13:34:01 +0300

    Hide create storage from viewer

--- frontend/src/components/storage/Storages.tsx
+++ frontend/src/components/storage/Storages.tsx
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { type Login, selectLogin } from '../../store/login/reducer'
 import { useListStoragesQuery } from '../../store/storage/api'
 import { type Storage } from '../../store/storage/types'
+import { Role } from '../../store/user/types'
 
 import LoadingIndicator from '../common/LoadingIndicator'
 import { formatBestBefore, joinSortedNames } from '../util'
@@ -12,6 +15,8 @@ import './Storages.css'
 
 function Storages (): JSX.Element {
   const { data: storageData, isLoading } = useListStoragesQuery()
+  const login: Login = useSelector(selectLogin)
+  const isAdmin = login?.user?.role === Role.admin
 
   const storages = storageData?.storages === undefined
     ? []
@@ -26,7 +31,7 @@ function Storages (): JSX.Element {
           <div className='BeerBreweries'>Breweries</div>
           <div className='BeerStyles'>Styles</div>
           <div className='BestBefore'>Best before</div>
-          <div className='ReviewLink'></div>
+          {isAdmin && <div className='ReviewLink'></div>}
       </div>
       {storages.map((storage: Storage) => (
         <div className='StorageRow RowLike' key={storage.id}>
@@ -40,15 +45,19 @@ function Storages (): JSX.Element {
           <div className='BestBefore'>
             {formatBestBefore(storage.bestBefore)}
           </div>
-          <div className='ReviewLink'>
-            <Link to={`/addreview/${storage.id}`}>Review</Link>
-          </div>
+          {isAdmin && (
+            <div className='ReviewLink'>
+              <Link to={`/addreview/${storage.id}`}>Review</Link>
+            </div>
+          )}
         </div>
       ))}
       <hr/>
-      <div>
-        <CreateStorage />
-      </div>
+      {isAdmin && (
+        <div>
+          <CreateStorage />
+        </div>
+      )}
     </div>
   )
 }
