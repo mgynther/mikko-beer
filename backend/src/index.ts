f7e71d72e102adb8a0dc38d8be07c891e6638ad3
Mikko Gynther <mgynthercode@gmail.com>
2024-06-30 17:58:33 +0300

    Refactor index to use log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import { App } from './web/app'
 import { config } from './web/config'
 import { consoleLog as log } from './core/console-log'
+import { Level } from './core/log'
 
 const app = new App(config, log)
 
 app.start().then(() => {
-  console.log('App started')
+  log(Level.INFO, 'App started')
 }, () => {
-  console.warn('App promise rejected')
+  log(Level.ERROR, 'App promise rejected')
 })
