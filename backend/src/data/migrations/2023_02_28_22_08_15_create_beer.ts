20e8f27062444a049333539877d62341d90b7d3e
Mikko Gynther <mgynthercode@gmail.com>
2024-07-06 23:56:12 +0300

    Fix beer migration
    Not that it would matter much now but the migration that
    added beer had a terrible down function deleting style
    table.

--- backend/src/data/migrations/2023_02_28_22_08_15_create_beer.ts
+++ backend/src/data/migrations/2023_02_28_22_08_15_create_beer.ts
@@ -32,5 +32,5 @@ export async function up (db: Kysely<any>): Promise<void> {
 }
 
 export async function down (db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable('style').execute()
+  await db.schema.dropTable('beer').execute()
 }
