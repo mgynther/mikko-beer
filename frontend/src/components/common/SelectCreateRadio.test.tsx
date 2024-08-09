23adb18b800caab82e0b978335d5047322a277a4
Mikko Gynther <mgynthercode@gmail.com>
2024-08-09 23:10:47 +0300

    Add SelectCreateRadio tests

---
+++ frontend/src/components/common/SelectCreateRadio.test.tsx
@@ -0,0 +1,102 @@
+import { expect, test, vitest } from 'vitest'
+import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import SelectCreateRadio, { Mode, SelectCreateRadioBasic } from './SelectCreateRadio'
+
+test('basic, clicks create when already selected', async () => {
+  const user = userEvent.setup()
+  const onChange = vitest.fn()
+  const { getByRole } = render(
+    <SelectCreateRadioBasic
+      mode={Mode.CREATE}
+      onChange={onChange}
+    />
+  )
+  const create = getByRole('radio', { name: 'Create' })
+  expect(create).toBeDefined()
+  expect((create as HTMLInputElement).checked).toEqual(true)
+  await user.click(create)
+  expect(onChange.mock.calls).toEqual([])
+})
+
+test('basic, clicks select when already selected', async () => {
+  const user = userEvent.setup()
+  const onChange = vitest.fn()
+  const { getByRole } = render(
+    <SelectCreateRadioBasic
+      mode={Mode.SELECT}
+      onChange={onChange}
+    />
+  )
+  const select = getByRole('radio', { name: 'Select' })
+  expect((select as HTMLInputElement).checked).toEqual(true)
+  expect(select).toBeDefined()
+  await user.click(select)
+  expect(onChange.mock.calls).toEqual([])
+})
+
+test('basic, clicks create', async () => {
+  const user = userEvent.setup()
+  const onChange = vitest.fn()
+  const { getByRole } = render(
+    <SelectCreateRadioBasic
+      mode={Mode.SELECT}
+      onChange={onChange}
+    />
+  )
+  const create = getByRole('radio', { name: 'Create' })
+  expect(create).toBeDefined()
+  expect((create as HTMLInputElement).checked).toEqual(false)
+  await user.click(create)
+  expect(onChange.mock.calls).toEqual([[Mode.CREATE]])
+})
+
+test('basic, clicks select', async () => {
+  const user = userEvent.setup()
+  const onChange = vitest.fn()
+  const { getByRole } = render(
+    <SelectCreateRadioBasic
+      mode={Mode.CREATE}
+      onChange={onChange}
+    />
+  )
+  const select = getByRole('radio', { name: 'Select' })
+  expect(select).toBeDefined()
+  expect((select as HTMLInputElement).checked).toEqual(false)
+  await user.click(select)
+  expect(onChange.mock.calls).toEqual([[Mode.SELECT]])
+})
+
+test('full, changes mode', async () => {
+  const user = userEvent.setup()
+  const createTextValue = 'This is create'
+  const selectTextValue = 'This is select'
+  const { getByRole, getByText, queryByText } = render(
+    <SelectCreateRadio
+      defaultMode={Mode.SELECT}
+      createElement={<div>{createTextValue}</div>}
+      selectElement={<div>{selectTextValue}</div>}
+    />
+  )
+  expect(queryByText(createTextValue)).toBeNull()
+  expect(getByText(selectTextValue)).toBeDefined()
+
+  const create = getByRole('radio', { name: 'Create' })
+  expect(create).toBeDefined()
+  expect((create as HTMLInputElement).checked).toEqual(false)
+  await user.click(create)
+  const createText = getByText(createTextValue)
+  expect(createText).toBeDefined()
+  const selectText = queryByText(selectTextValue)
+  expect(selectText).toEqual(null)
+
+  const select = getByRole('radio', { name: 'Select' })
+  expect(select).toBeDefined()
+  expect((select as HTMLInputElement).checked).toEqual(false)
+  await user.click(select)
+  const secondCreateText = queryByText(createTextValue)
+  expect(secondCreateText).toEqual(null)
+  const secondSelectText = getByText(selectTextValue)
+  expect(secondSelectText).toBeDefined()
+})
