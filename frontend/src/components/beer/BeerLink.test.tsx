42b5136b99a6896aa9de1e65af0b8a381e2d4f11
Mikko Gynther <mgynthercode@gmail.com>
2024-10-04 23:33:32 +0300

    Add BeerLink test

---
+++ frontend/src/components/beer/BeerLink.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react'
+import { test } from 'vitest'
+import LinkWrapper from '../LinkWrapper'
+
+import BeerLink from './BeerLink'
+
+test('renders brewery links', () => {
+  const beerName = 'Siperia'
+  const { getByRole } = render(
+    <LinkWrapper>
+      <BeerLink
+        beer={{
+          id: 'b71b0c12-0538-48ca-9c82-75e84caca818',
+          name: beerName
+        }}
+      />
+    </LinkWrapper>
+  )
+  getByRole('link', { name: beerName })
+})
