159b3c9b478bd89fff777c27a56eb280b91ad49e
Mikko Gynther <mgynthercode@gmail.com>
2024-12-06 23:53:16 +0200

    Refactor stats authorization to core

---
+++ backend/src/core/stats/authorized-stats.service.ts
@@ -0,0 +1,90 @@
+import * as authService from '../../core/authentication/authentication.service'
+import * as statsService from '../../core/stats/stats.service'
+
+import type {
+  AuthTokenPayload
+} from "../authentication/auth-token"
+import type {
+  AnnualStats,
+  BreweryStats,
+  BreweryStatsOrder,
+  OverallStats,
+  RatingStats,
+  StatsBreweryStyleFilter,
+  StatsFilter,
+  StyleStats,
+  StyleStatsOrder
+} from "./stats"
+import type { log } from '../log'
+import type { Pagination } from '../pagination'
+
+export async function getAnnual (
+  getAnnual: (statsFilter: StatsBreweryStyleFilter) => Promise<AnnualStats>,
+  authTokenPayload: AuthTokenPayload,
+  statsFilter: StatsBreweryStyleFilter,
+  log: log
+): Promise<AnnualStats> {
+  authService.authenticateViewerPayload(authTokenPayload)
+  return await statsService.getAnnual(getAnnual, statsFilter, log)
+}
+
+export async function getBrewery (
+  getBrewery: (
+    pagination: Pagination,
+    statsFilter: StatsFilter,
+    breweryStatsOrder: BreweryStatsOrder
+  ) => Promise<BreweryStats>,
+  authTokenPayload: AuthTokenPayload,
+  pagination: Pagination,
+  statsFilter: StatsFilter,
+  breweryStatsOrder: BreweryStatsOrder,
+  log: log
+): Promise<BreweryStats> {
+  authService.authenticateViewerPayload(authTokenPayload)
+  return await statsService.getBrewery(
+    getBrewery,
+    pagination,
+    statsFilter,
+    breweryStatsOrder,
+    log
+  )
+}
+
+export async function getOverall (
+  getOverall: (statsFilter: StatsBreweryStyleFilter) => Promise<OverallStats>,
+  authTokenPayload: AuthTokenPayload,
+  statsFilter: StatsBreweryStyleFilter,
+  log: log
+): Promise<OverallStats> {
+  authService.authenticateViewerPayload(authTokenPayload)
+  return await statsService.getOverall(getOverall, statsFilter, log)
+}
+
+export async function getRating (
+  getRating: (statsFilter: StatsBreweryStyleFilter) => Promise<RatingStats>,
+  authTokenPayload: AuthTokenPayload,
+  statsFilter: StatsBreweryStyleFilter,
+  log: log
+): Promise<RatingStats> {
+  authService.authenticateViewerPayload(authTokenPayload)
+  return await statsService.getRating(getRating, statsFilter, log)
+}
+
+export async function getStyle (
+  getStyle: (
+    statsFilter: StatsFilter,
+    styleStatsOrder: StyleStatsOrder
+  ) => Promise<StyleStats>,
+  authTokenPayload: AuthTokenPayload,
+  statsFilter: StatsFilter,
+  styleStatsOrder: StyleStatsOrder,
+  log: log
+): Promise<StyleStats> {
+  authService.authenticateViewerPayload(authTokenPayload)
+  return await statsService.getStyle(
+    getStyle,
+    statsFilter,
+    styleStatsOrder,
+    log
+  )
+}
--- backend/src/web/stats/stats.controller.ts
+++ backend/src/web/stats/stats.controller.ts
@@ -1,54 +1,56 @@
 import * as statsRepository from '../../data/stats/stats.repository'
-import * as statsService from '../../core/stats/stats.service'
-import * as authHelper from '../authentication/authentication-helper'
+import * as statsService from '../../core/stats/authorized-stats.service'
 
-import { type Pagination } from '../../core/pagination'
-import { type Router } from '../router'
+import type { Pagination } from '../../core/pagination'
+import type { Router } from '../router'
 
 import { validatePagination} from '../../core/pagination'
 
+import type {
+  BreweryStatsOrder,
+  StyleStatsOrder,
+  StatsBreweryStyleFilter,
+  StatsFilter,
+} from '../../core/stats/stats'
 import {
-  type BreweryStatsOrder,
   validateBreweryStatsOrder,
-  type StyleStatsOrder,
   validateStyleStatsOrder,
-  type StatsBreweryStyleFilter,
-  type StatsFilter,
   validateStatsBreweryStyleFilter,
   validateStatsFilter
 } from '../../core/stats/stats'
+import { parseAuthToken } from '../authentication/authentication-helper'
 
 export function statsController (router: Router): void {
   router.get(
     '/api/v1/stats/overall',
-    authHelper.authenticateViewer,
     async (ctx) => {
+      const authTokenPayload = parseAuthToken(ctx)
       const statsFilter = validateStatsBreweryStyleFilter(ctx.request.query)
       const overall = await statsService.getOverall((
         statsFilter: StatsBreweryStyleFilter
       ) => {
         return statsRepository.getOverall(ctx.db, statsFilter)
-      }, statsFilter, ctx.log)
+      }, authTokenPayload, statsFilter, ctx.log)
       ctx.body = { overall }
     }
   )
   router.get(
     '/api/v1/stats/annual',
-    authHelper.authenticateViewer,
     async (ctx) => {
+      const authTokenPayload = parseAuthToken(ctx)
       const statsFilter = validateStatsBreweryStyleFilter(ctx.request.query)
       const annual = await statsService.getAnnual((
         statsFilter: StatsBreweryStyleFilter
       ) => {
         return statsRepository.getAnnual(ctx.db, statsFilter)
-      }, statsFilter, ctx.log)
+      }, authTokenPayload, statsFilter, ctx.log)
       ctx.body = { annual }
     }
   )
   router.get(
     '/api/v1/stats/brewery',
-    authHelper.authenticateViewer,
     async (ctx) => {
+      const authTokenPayload = parseAuthToken(ctx)
       const { skip, size } = ctx.request.query
       const { order, direction } = ctx.request.query
       const pagination = validatePagination({ skip, size })
@@ -65,6 +67,7 @@ export function statsController (router: Router): void {
               ctx.db, pagination, statsFilter, breweryStatsOrder
             )
           },
+          authTokenPayload,
           pagination,
           statsFilter,
           breweryStatsOrder,
@@ -76,22 +79,22 @@ export function statsController (router: Router): void {
 
   router.get(
     '/api/v1/stats/rating',
-    authHelper.authenticateViewer,
     async (ctx) => {
+      const authTokenPayload = parseAuthToken(ctx)
       const statsFilter = validateStatsBreweryStyleFilter(ctx.request.query)
       const rating = await statsService.getRating((
         statsFilter: StatsBreweryStyleFilter
       ) => {
         return statsRepository.getRating(ctx.db, statsFilter)
-      }, statsFilter, ctx.log)
+      }, authTokenPayload, statsFilter, ctx.log)
       ctx.body = { rating }
     }
   )
 
   router.get(
     '/api/v1/stats/style',
-    authHelper.authenticateViewer,
     async (ctx) => {
+      const authTokenPayload = parseAuthToken(ctx)
       const { order, direction } = ctx.request.query
       const styleStatsOrder = validateStyleStatsOrder({ order, direction })
       const statsFilter = validateStatsFilter(ctx.request.query)
@@ -103,7 +106,7 @@ export function statsController (router: Router): void {
           ctx.db,
           statsFilter,
           styleStatsOrder
-        )}, statsFilter, styleStatsOrder, ctx.log)
+        )}, authTokenPayload, statsFilter, styleStatsOrder, ctx.log)
       ctx.body = { style }
     }
   )
