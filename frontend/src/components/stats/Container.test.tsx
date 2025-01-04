6663deea168ff462605cf3ee248ab231d877a8b8
Mikko Gynther <mgynthercode@gmail.com>
2025-01-04 16:52:27 +0200

    Sort and filter container stats

--- frontend/src/components/stats/Container.test.tsx
+++ frontend/src/components/stats/Container.test.tsx
@@ -1,35 +1,46 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { expect, test, vitest } from 'vitest'
 import Container from './Container'
-import type { BreweryStyleParams } from '../../core/stats/types'
+import type {
+  BreweryStyleParams,
+  OneContainerStats
+} from '../../core/stats/types'
+import { openFilters } from './filters-test-util'
 
-test('renders container stats', () => {
-  const stats = vitest.fn()
-  const breweryId = 'e6887360-78da-49e2-b876-68477c79c776'
-  const styleId = '2b885977-a2fd-43c2-95f9-6b19f3c8054d'
-  const { getByText } = render(
+const breweryId = 'e6887360-78da-49e2-b876-68477c79c776'
+const styleId = '2b885977-a2fd-43c2-95f9-6b19f3c8054d'
+
+const containerStats: OneContainerStats[] = [
+  {
+    containerId: 'f6937cf3-e0fa-4c6b-92b5-f374242342f6',
+    containerSize: '0.25',
+    containerType: 'draft',
+    reviewAverage: '7.87',
+    reviewCount: '24'
+  },
+  {
+    containerId: 'f908dc6a-3ed7-49e1-8caf-6ae1b9aac4ff',
+    containerSize: '0.33',
+    containerType: 'bottle',
+    reviewAverage: '8.23',
+    reviewCount: '10'
+  }
+]
+
+function renderContainer (
+  stats?: ReturnType<typeof vitest.fn>
+): ReturnType<typeof render> {
+  return render(
     <Container
       getContainerStatsIf={{
         useStats: (params: BreweryStyleParams) => {
-          stats(params)
+          if (stats !== undefined) {
+            stats(params)
+          }
           return {
             stats: {
-              container: [
-                {
-                  containerId: 'f6937cf3-e0fa-4c6b-92b5-f374242342f6',
-                  containerSize: '0.25',
-                  containerType: 'draft',
-                  reviewAverage: '7.87',
-                  reviewCount: '10'
-                },
-                {
-                  containerId: 'f908dc6a-3ed7-49e1-8caf-6ae1b9aac4ff',
-                  containerSize: '0.33',
-                  containerType: 'bottle',
-                  reviewAverage: '8.23',
-                  reviewCount: '24'
-                }
-              ]
+              container: containerStats
             },
             isLoading: false
           }
@@ -39,6 +50,11 @@ test('renders container stats', () => {
       styleId={styleId}
     />
   )
+}
+
+test('renders container stats', () => {
+  const stats = vitest.fn()
+  const { getByText } = renderContainer(stats)
   expect(stats.mock.calls).toEqual([[{ breweryId, styleId }]])
   getByText('7.87')
   getByText('10')
@@ -46,4 +62,131 @@ test('renders container stats', () => {
   getByText('8.23')
   getByText('24')
   getByText('bottle 0.33')
+})
+
+function changeSlider (
+  getByDisplayValue: (str: string) => HTMLElement,
+  from: string,
+  to: string
+): void {
+  const slider = getByDisplayValue(from)
+  fireEvent.change(slider, {target: { value: to }})
+}
+
+test('filter container stats by min average', async () => {
+  const user = userEvent.setup()
+  const { getByDisplayValue, getByRole, getByText, queryByText } =
+    renderContainer()
+  await openFilters(getByRole, user)
+  changeSlider(getByDisplayValue, '4', '8.1')
+  expect(queryByText('7.87')).toEqual(null)
+  getByText('8.23')
+})
+
+test('filter container stats by max average', async () => {
+  const user = userEvent.setup()
+  const { getByDisplayValue, getByRole, getByText, queryByText } =
+    renderContainer()
+  await openFilters(getByRole, user)
+  changeSlider(getByDisplayValue, '10', '8.0')
+  getByText('7.87')
+  expect(queryByText('8.23')).toEqual(null)
+})
+
+test('filter container stats by min count', async () => {
+  const user = userEvent.setup()
+  const { getByDisplayValue, getByRole, getByText, queryByText } =
+    renderContainer()
+  await openFilters(getByRole, user)
+  changeSlider(getByDisplayValue, '0', '5')
+  expect(queryByText('10')).toEqual(null)
+  getByText('24')
+})
+
+test('filter container stats by min count', async () => {
+  const user = userEvent.setup()
+  const { getByDisplayValue, getByRole, getByText, queryByText } =
+    renderContainer()
+  await openFilters(getByRole, user)
+  changeSlider(getByDisplayValue, '11', '5')
+  getByText('10')
+  expect(queryByText('24')).toEqual(null)
+})
+
+test('order container stats by average desc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const averageButton = getByRole('button', { name: 'Average' })
+  await user.click(averageButton)
+  const averages = getAllByText(/7.87|8.23/)
+  expect(averages.length).toEqual(2)
+  expect(averages[0].innerHTML).toEqual('8.23')
+  expect(averages[1].innerHTML).toEqual('7.87')
+})
+
+test('order container stats by average asc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const averageButton = getByRole('button', { name: 'Average' })
+  await user.click(averageButton)
+  const averageAscButton = getByRole('button', { name: 'Average ▼' })
+  await user.click(averageAscButton)
+  const averages = getAllByText(/7.87|8.23/)
+  expect(averages.length).toEqual(2)
+  expect(averages[0].innerHTML).toEqual('7.87')
+  expect(averages[1].innerHTML).toEqual('8.23')
+})
+
+test('order container stats by count desc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const averageButton = getByRole('button', { name: 'Reviews' })
+  await user.click(averageButton)
+  const counts = getAllByText(/10|24/)
+  expect(counts.length).toEqual(2)
+  expect(counts[0].innerHTML).toEqual('24')
+  expect(counts[1].innerHTML).toEqual('10')
+})
+
+test('order container stats by average asc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const countButton = getByRole('button', { name: 'Reviews' })
+  await user.click(countButton)
+  const countAscButton = getByRole('button', { name: 'Reviews ▼' })
+  await user.click(countAscButton)
+  const counts = getAllByText(/10|24/)
+  expect(counts.length).toEqual(2)
+  expect(counts[0].innerHTML).toEqual('10')
+  expect(counts[1].innerHTML).toEqual('24')
+})
+
+test('order container stats by container desc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const textButton = getByRole('button', { name: 'Container ▲' })
+  await user.click(textButton)
+  const containers = getAllByText(/draft 0.25|bottle 0.33/)
+  expect(containers.length).toEqual(2)
+  expect(containers[0].innerHTML).toEqual('draft 0.25')
+  expect(containers[1].innerHTML).toEqual('bottle 0.33')
+})
+
+test('order container stats by container asc', async () => {
+  const user = userEvent.setup()
+  const { getAllByText, getByRole } =
+    renderContainer()
+  const textButton = getByRole('button', { name: 'Container ▲' })
+  await user.click(textButton)
+  const textAscButton = getByRole('button', { name: 'Container ▼' })
+  await user.click(textAscButton)
+  const containers = getAllByText(/draft 0.25|bottle 0.33/)
+  expect(containers.length).toEqual(2)
+  expect(containers[0].innerHTML).toEqual('bottle 0.33')
+  expect(containers[1].innerHTML).toEqual('draft 0.25')
 })
--- frontend/src/components/stats/Container.tsx
+++ frontend/src/components/stats/Container.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import type { GetContainerStatsIf } from '../../core/stats/types'
+import type {
+  GetContainerStatsIf,
+  OneContainerStats
+} from '../../core/stats/types'
 
 import LoadingIndicator from '../common/LoadingIndicator'
 
 import './StatsTable.css'
 import { asText } from '../container/ContainerInfo'
+import Filters from './Filters'
+import TabButton from '../common/TabButton'
+import type { ListDirection } from '../../core/types'
+import { formatTitle, invertDirection } from '../list-helpers'
 
 interface Props {
   getContainerStatsIf: GetContainerStatsIf
@@ -13,34 +20,193 @@ interface Props {
   styleId: string | undefined
 }
 
+function num (str: string): number {
+  return parseFloat(str)
+}
+
+type SortingOrder = 'text' | 'count' | 'average'
+
+interface NumContainer {
+  id: string
+  text: string
+  count: number
+  average: number
+}
+
+interface VisualContainer {
+  id: string
+  text: string
+  count: string
+  average: string
+}
+
+function toNum (container: OneContainerStats): NumContainer {
+  return {
+    id: container.containerId,
+    text: asText({
+      id: container.containerId,
+      size: container.containerSize,
+      type: container.containerType
+    }),
+    count: num(container.reviewCount),
+    average: num(container.reviewAverage)
+  }
+}
+
+function toVisual (container: NumContainer): VisualContainer {
+  return {
+    id: container.id,
+    text: container.text,
+    count: container.count.toFixed(0),
+    average: container.average.toFixed(2)
+  }
+}
+
+type Sorter = (a: NumContainer, b: NumContainer) => number
+
+const textAscSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  a.text.localeCompare(b.text)
+const textDescSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  b.text.localeCompare(a.text)
+const averageAscSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  a.average - b.average
+const averageDescSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  b.average - a.average
+const countAscSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  a.count - b.count
+const countDescSorter: Sorter = (a: NumContainer, b: NumContainer) =>
+  b.count - a.count
+
+function getSorter (order: SortingOrder, direction: ListDirection): Sorter {
+  switch (order) {
+  case 'text':
+    return direction === 'asc' ? textAscSorter : textDescSorter
+  case 'average':
+    return direction === 'asc' ? averageAscSorter : averageDescSorter
+  case 'count':
+    return direction === 'asc' ? countAscSorter : countDescSorter
+  }
+}
+
 function Container (props: Props): React.JSX.Element {
   const { stats, isLoading } = props.getContainerStatsIf.useStats({
     breweryId: props.breweryId,
     styleId: props.styleId
   })
+
+  const [
+    sortingOrder,
+    setSortingOrder
+  ] = useState<SortingOrder>('text')
+  const [
+    sortingDirection,
+    setSortingDirection
+  ] = useState<ListDirection>('asc')
+
+  function isSelected (property: SortingOrder): boolean {
+    return sortingOrder === property
+  }
+
+  function createClickHandler (property: SortingOrder): () => void {
+    return () => {
+      if (isSelected(property)) {
+        setSortingDirection(invertDirection(sortingDirection))
+        return
+      }
+      setSortingOrder(property)
+      setSortingDirection(property === 'text' ? 'asc' : 'desc')
+    }
+  }
+
+  const [minReviewCount, setMinReviewCount] = useState(1)
+  const [maxReviewCount, setMaxReviewCount] = useState(Infinity)
+  const [minReviewAverage, setMinReviewAverage] = useState(4)
+  const [maxReviewAverage, setMaxReviewAverage] = useState(10)
+
   return (
     <div>
       <LoadingIndicator isLoading={isLoading} />
-      <table className='StatsTable'>
+      <table className='StatsTable SortableStats'>
         <thead>
           <tr>
-            <th>Container</th>
-            <th>Reviews</th>
-            <th>Review rating average</th>
+            <th className='StatsNameColumn'>
+              <TabButton
+                isCompact={false}
+                isSelected={isSelected('text')}
+                title={formatTitle(
+                  'Container',
+                  isSelected('text'),
+                  sortingDirection
+                )}
+                onClick={createClickHandler('text')} />
+            </th>
+            <th className='StatsNumColumn'>
+              <TabButton
+                isCompact={false}
+                isSelected={isSelected('count')}
+                title={formatTitle(
+                  'Reviews',
+                  isSelected('count'),
+                  sortingDirection
+                )}
+                onClick={createClickHandler('count')} />
+            </th>
+            <th className='StatsNumColumn'>
+              <TabButton
+                isCompact={false}
+                isSelected={isSelected('average')}
+                title={formatTitle(
+                  'Average',
+                  isSelected('average'),
+                  sortingDirection
+                )}
+                onClick={createClickHandler('average')} />
+            </th>
+          </tr>
+          <tr>
+            <th colSpan={3}>
+              <Filters
+                filters={{
+                  minReviewCount: {
+                    value: minReviewCount,
+                    setValue: setMinReviewCount
+                  },
+                  maxReviewCount: {
+                    value: maxReviewCount,
+                    setValue: setMaxReviewCount
+                  },
+                  minReviewAverage: {
+                    value: minReviewAverage,
+                    setValue: setMinReviewAverage
+                  },
+                  maxReviewAverage: {
+                    value: maxReviewAverage,
+                    setValue: setMaxReviewAverage
+                  }
+                }}
+              />
+            </th>
           </tr>
         </thead>
         <tbody>
-          {stats?.container.map(container => (
-            <tr key={container.containerId}>
-              <td>{asText({
-                id: container.containerId,
-                size: container.containerSize,
-                type: container.containerType
-              })}</td>
-              <td>{container.reviewCount}</td>
-              <td>{container.reviewAverage}</td>
-            </tr>
-          ))}
+          {stats?.container.map(toNum)
+            .filter(
+              container =>
+                container.average >= minReviewAverage
+                && container.average <= maxReviewAverage
+                && container.count >= minReviewCount
+                && container.count <= maxReviewCount
+            ).sort(
+              getSorter(sortingOrder, sortingDirection)
+            ).map(
+              toVisual
+            ).map(container => (
+              <tr key={container.id}>
+                <td>{container.text}</td>
+                <td>{container.count}</td>
+                <td>{container.average}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
