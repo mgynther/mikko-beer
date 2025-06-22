4baa89a28ce3980ce31e463bb3bdb8a76ba34559
Mikko Gynther <mgynthercode@gmail.com>
2025-06-22 14:35:38 +0300

    Add annual data tests

--- backend/test/data/stats/annual.test.ts
+++ backend/test/data/stats/annual.test.ts
@@ -8,7 +8,11 @@ import type { Pagination } from '../../../src/core/pagination'
 
 const giantPage: Pagination = { size: 10000, skip: 0 }
 
-describe('annual container stats tests', () => {
+function filterByContainer(reviews: Review[], containerId: string): Review[] {
+  return reviews.filter(r => r.container === containerId)
+}
+
+describe('annual stats tests', () => {
   const ctx = new TestContext()
 
   before(ctx.before)
@@ -17,9 +21,111 @@ describe('annual container stats tests', () => {
   after(ctx.after)
   afterEach(ctx.afterEach)
 
-  function filterByContainer(reviews: Review[], containerId: string): Review[] {
-    return reviews.filter(r => r.container === containerId)
+  function filterByYear(reviews: Review[], year: string): Review[] {
+    return reviews.filter(r => `${new Date(r.time).getUTCFullYear()}` === year)
   }
+
+  function avg(reviews: Review[], year: string) {
+    if (reviews === null) throw new Error('must not be null')
+    const filteredReviews = filterByYear(reviews, year)
+    const sum = filteredReviews
+      .map(r => r.rating)
+      .reduce<number>((sum, rating) => sum + (rating ?? 0), 0)
+
+    const numValue = sum / filteredReviews.length
+    return numValue.toFixed(2)
+  }
+
+  it('no filters', async () => {
+    const { reviews } = await insertMultipleReviews(9, ctx.db)
+    const stats = await statsRepository.getAnnual(
+      ctx.db,
+      {
+        brewery: undefined,
+        location: undefined,
+        style: undefined
+      }
+    )
+    expect(stats).toEqual([
+      {
+        reviewAverage: avg(reviews, '2023'),
+        reviewCount: `${filterByYear(reviews, '2023').length}`,
+        year: '2023'
+      },
+      {
+        reviewAverage: avg(reviews, '2024'),
+        reviewCount: `${filterByYear(reviews, '2024').length}`,
+        year: '2024'
+      }
+    ])
+  })
+
+  it('filter by brewery', async () => {
+    const { data, reviews } = await insertMultipleReviews(9, ctx.db)
+    const stats = await statsRepository.getAnnual(
+      ctx.db,
+      {
+        brewery: data.brewery.id,
+        location: undefined,
+        style: undefined
+      }
+    )
+    expect(stats).toEqual([
+      {
+        reviewAverage: avg(reviews, '2024'),
+        reviewCount: `${filterByYear(reviews, '2024').length}`,
+        year: '2024'
+      }
+    ])
+  })
+
+  it('filter by location', async () => {
+    const { data, reviews } = await insertMultipleReviews(9, ctx.db)
+    const stats = await statsRepository.getAnnual(
+      ctx.db,
+      {
+        brewery: undefined,
+        location: data.location.id,
+        style: undefined
+      }
+    )
+    expect(stats).toEqual([
+      {
+        reviewAverage: avg(reviews, '2024'),
+        reviewCount: `${filterByYear(reviews, '2024').length}`,
+        year: '2024'
+      }
+    ])
+  })
+
+  it('filter by style', async () => {
+    const { data } = await insertMultipleReviews(9, ctx.db)
+    const stats = await statsRepository.getAnnual(
+      ctx.db,
+      {
+        brewery: undefined,
+        location: undefined,
+        style: data.otherStyle.id
+      }
+    )
+    expect(stats).toEqual([
+      {
+        reviewAverage: '6.60',
+        reviewCount: '5',
+        year: '2023'
+      }
+    ])
+  })
+})
+
+describe('annual container stats tests', () => {
+  const ctx = new TestContext()
+
+  before(ctx.before)
+  beforeEach(ctx.beforeEach)
+
+  after(ctx.after)
+  afterEach(ctx.afterEach)
 
   function avg(reviews: Review[], containerId: string) {
     if (reviews === null) throw new Error('must not be null')
