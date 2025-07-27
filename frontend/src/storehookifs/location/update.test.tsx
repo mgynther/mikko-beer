a47abf1c6d727cd228a87240f500897903bc2995
Mikko Gynther <mgynthercode@gmail.com>
2025-07-27 20:08:33 +0300

    Test update location

---
+++ frontend/src/storehookifs/location/update.test.tsx
@@ -0,0 +1,70 @@
+import { test, vitest } from 'vitest'
+import { store } from '../../store/store'
+import { addTestServerResponse } from '../../../test-util/server'
+import updateLocation from './update'
+import type { Location } from '../../core/location/types'
+import { render, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Provider } from '../../react-redux-wrapper'
+
+interface HelperProps {
+  location: Location
+  handleResponse: (location: Location) => void
+}
+
+function Helper(props: HelperProps): React.JSX.Element {
+  const updateIf = updateLocation(() => ({
+    user: undefined,
+    authToken: '',
+    refreshToken: ''
+  }))
+  const update = updateIf.useUpdate()
+  const handleClick = (): void => {
+    async function doHandle(): Promise<void> {
+      await update.update(props.location)
+    }
+    void doHandle()
+  }
+  return (
+    <>
+      <button onClick={handleClick}>Test</button>
+      {update.isLoading && <div>Loading</div>}
+      {!update.isLoading && <div>Not loading</div>}
+    </>
+  )
+}
+
+test('update location', async () => {
+  const user = userEvent.setup()
+
+  const expectedResponse = {
+    location: {
+      id: '640ced88-a5b4-498e-9007-a045e0cc1797',
+      name: 'Test location'
+    }
+  }
+
+  addTestServerResponse<{location: Location}>({
+    method: 'PUT',
+    pathname: `/api/v1/location/${expectedResponse.location.id}`,
+    response: expectedResponse,
+    status: 200
+  })
+
+  const handler = vitest.fn()
+  const { getByRole, getByText } = render(
+    <Provider store={store}>
+      <Helper location={expectedResponse.location} handleResponse={
+        handler
+      } />
+    </Provider>
+  )
+  const testButton = getByRole('button', { name: 'Test' })
+  await user.click(testButton)
+  await waitFor(() => {
+    getByText('Loading')
+  })
+  await waitFor(() => {
+    getByText('Not loading')
+  })
+})
