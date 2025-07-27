9cd97cb6efb1683ef40020be6a8071a5db12c609
Mikko Gynther <mgynthercode@gmail.com>
2025-07-27 19:57:24 +0300

    Test create location

---
+++ frontend/src/storehookifs/location/create.test.tsx
@@ -0,0 +1,63 @@
+import { expect, test, vitest } from 'vitest'
+import { store } from '../../store/store'
+import { addTestServerResponse } from '../../../test-util/server'
+import createLocation from './create'
+import type { Location, CreateLocationRequest } from '../../core/location/types'
+import { render, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Provider } from '../../react-redux-wrapper'
+
+interface HelperProps {
+  location: CreateLocationRequest
+  handleResponse: (location: Location) => void
+}
+
+function Helper(props: HelperProps): React.JSX.Element {
+  const createIf = createLocation()
+  const create = createIf.useCreate()
+  const handleClick = (): void => {
+    async function doHandle(): Promise<void> {
+      const response = await create.create(props.location)
+      props.handleResponse(response)
+    }
+    void doHandle()
+  }
+  return (
+    <button onClick={handleClick}>Test</button>
+  )
+}
+
+test('create location', async () => {
+  const user = userEvent.setup()
+
+  const expectedResponse = {
+    location: {
+      id: 'c053e116-53e3-4de4-8273-e35158d776b7',
+      name: 'Test location'
+    }
+  }
+
+  addTestServerResponse<{location: Location}>({
+    method: 'POST',
+    pathname: '/api/v1/location',
+    response: expectedResponse,
+    status: 201
+  })
+
+  const handler = vitest.fn()
+  const { getByRole } = render(
+    <Provider store={store}>
+      <Helper
+        location={{ name: expectedResponse.location.name }}
+        handleResponse={
+          handler
+        }
+      />
+    </Provider>
+  )
+  const testButton = getByRole('button', { name: 'Test' })
+  await user.click(testButton)
+  await waitFor(() => {
+    expect(handler).toHaveBeenCalledWith(expectedResponse.location)
+  })
+})
