4789b358237784e65b8faee0cf20d7fbde6028ea
Mikko Gynther <mgynthercode@gmail.com>
2025-07-27 20:28:00 +0300

    Test update container

---
+++ frontend/src/storehookifs/container/update.test.tsx
@@ -0,0 +1,67 @@
+import { test, vitest } from 'vitest'
+import { store } from '../../store/store'
+import { addTestServerResponse } from '../../../test-util/server'
+import updateContainer from './update'
+import type { Container } from '../../core/container/types'
+import { render, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Provider } from '../../react-redux-wrapper'
+
+interface HelperProps {
+  container: Container
+  handleResponse: (container: Container) => void
+}
+
+function Helper(props: HelperProps): React.JSX.Element {
+  const updateIf = updateContainer()
+  const update = updateIf.useUpdate()
+  const handleClick = (): void => {
+    async function doHandle(): Promise<void> {
+      await update.update(props.container)
+    }
+    void doHandle()
+  }
+  return (
+    <>
+      <button onClick={handleClick}>Test</button>
+      {update.isLoading && <div>Loading</div>}
+      {!update.isLoading && <div>Not loading</div>}
+    </>
+  )
+}
+
+test('update container', async () => {
+  const user = userEvent.setup()
+
+  const expectedResponse = {
+    container: {
+      id: 'ed8cfcd6-549e-45e5-901e-db2f9925856c',
+      type: 'bottle',
+      size: '0.50'
+    }
+  }
+
+  addTestServerResponse<{container: Container}>({
+    method: 'PUT',
+    pathname: `/api/v1/container/${expectedResponse.container.id}`,
+    response: expectedResponse,
+    status: 200
+  })
+
+  const handler = vitest.fn()
+  const { getByRole, getByText } = render(
+    <Provider store={store}>
+      <Helper container={expectedResponse.container} handleResponse={
+        handler
+      } />
+    </Provider>
+  )
+  const testButton = getByRole('button', { name: 'Test' })
+  await user.click(testButton)
+  await waitFor(() => {
+    getByText('Loading')
+  })
+  await waitFor(() => {
+    getByText('Not loading')
+  })
+})
