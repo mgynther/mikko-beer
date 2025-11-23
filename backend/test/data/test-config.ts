2a8db7f6ce2e6151e2ed973c8b6d95a8886fb2c3
Mikko Gynther <mgynthercode@gmail.com>
2025-11-23 16:07:48 +0200

    Read test db config from env

--- backend/test/data/test-config.ts
+++ backend/test/data/test-config.ts
@@ -1,12 +1,23 @@
 import type { ConnectionConfig } from 'pg'
 
 export const testConfig: ConnectionConfig = {
-  host: 'localhost',
-  database: 'mikko_beer_test',
-  user: 'postgres',
+  host: getEnvVariable('DATABASE_HOST'),
+  database: getEnvVariable('DATABASE'),
+  user: getEnvVariable('DATABASE_USER'),
+  password: getEnvVariable('DATABASE_PASSWORD')
 }
 export const testAdminConfig: ConnectionConfig = {
-  host: 'localhost',
+  host: getEnvVariable('DATABASE_HOST'),
   database: 'postgres',
-  user: 'postgres',
+  user: getEnvVariable('DATABASE_USER'),
+  password: getEnvVariable('DATABASE_PASSWORD')
+}
+
+function getEnvVariable (name: string): string {
+  if (process.env[name] === undefined) {
+    throw new Error(`environment variable ${name} not found`)
+  }
+
+  const str: string = process.env[name]
+  return str
 }
