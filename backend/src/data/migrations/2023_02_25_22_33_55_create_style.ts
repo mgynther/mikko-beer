eb65dcf5a4b5323f0f73536b88ecb75d0d639f67
Mikko Gynther <mgynthercode@gmail.com>
2026-03-22 15:58:29 +0200

    Fix style down migration

--- backend/src/data/migrations/2023_02_25_22_33_55_create_style.ts
+++ backend/src/data/migrations/2023_02_25_22_33_55_create_style.ts
@@ -24,5 +24,6 @@ export async function up (db: Kysely<any>): Promise<void> {
 }
 
 export async function down (db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('style_relationship').execute()
   await db.schema.dropTable('style').execute()
 }
