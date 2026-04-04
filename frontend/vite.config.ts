325f228b33edeabba974f3beace24c5d6c543229
Mikko Gynther <mgynthercode@gmail.com>
2026-04-05 00:56:16 +0300

    Fix manifest in build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: 'manifest.json',
+  },
   plugins: [react()],
   server: {
     forwardConsole: true
