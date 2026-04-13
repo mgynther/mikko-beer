409394fb65f9300685ead3714d31f80a3868e3f8
Mikko Gynther <mgynthercode@gmail.com>
2026-04-13 20:52:08 +0300

    Add dummy test for RtkApp

---
+++ frontend/src/RtkApp.test.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { test } from "vitest";
+import { Provider } from "./react-redux-wrapper";
+import React from "react";
+import LinkWrapper from "./components/LinkWrapper";
+import RtkApp from "./RtkApp";
+
+import { store } from './store/store'
+
+test('render index', () => {
+  const { getByRole } = render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <LinkWrapper>
+          <RtkApp />
+        </LinkWrapper>
+      </React.StrictMode>
+    </Provider>
+  )
+  getByRole('heading', { name: 'Login' })
+})
