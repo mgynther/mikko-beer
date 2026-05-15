254fd30b6aa43b43be08bf7b104571dafffc3295
Mikko Gynther <mgynthercode@gmail.com>
2026-05-15 19:51:34 +0300

    Add storage creating e2e test

---
+++ frontend/e2e/add-storage.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+import { v4 as uuidv4 } from 'uuid'
+import { login } from './login'
+import { reviewContainer } from './constants'
+
+test('Add storage', async ({ page }) => {
+  await login(page)
+  await page.getByRole('link', { name: /storage/i }).click()
+
+  const beerName = `e2e test beer ${uuidv4()}`
+  await page.getByRole('textbox', { name: /name/i }).fill(beerName)
+  await page.getByRole('textbox', { name: /search brewery/i }).fill('a.lec')
+  await page.getByRole('button', { name: /a.lecoq/i }).click()
+  await page.getByRole('textbox', { name: /search style/i }).fill('lag')
+  await page.getByRole('button', { name: /^lager$/i }).click()
+  await page.getByRole('button', { name: /create beer/i }).click()
+  await page.locator('select').selectOption({ label: reviewContainer })
+  await page.getByRole('textbox', { name: /best before/i }).fill('2026-05-05')
+  await page.getByRole('button', { name: 'Create', exact: true }).click()
+  await expect(page.getByRole('textbox', { name: /name/i })).toBeEmpty()
+})
