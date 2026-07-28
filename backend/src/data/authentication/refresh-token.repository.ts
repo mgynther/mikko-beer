13ced0570f8d6d863be7679ab9cd5c408e450812
Mikko Gynther <mgynthercode@gmail.com>
2026-07-28 12:45:04 +0300

    backend: Duplicate DbRefreshToken to data for independence

--- backend/src/data/authentication/refresh-token.repository.ts
+++ backend/src/data/authentication/refresh-token.repository.ts
@@ -1,5 +1,9 @@
-import type { DbRefreshToken } from '../../core/auth/refresh-token.js'
 import type { Database, Transaction } from '../database.js'
+
+export interface DbRefreshToken {
+  id: string
+  userId: string
+}
 
 export async function insertRefreshToken(
   trx: Transaction,
